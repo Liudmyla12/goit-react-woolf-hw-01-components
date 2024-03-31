ad16169ce6d13040402c0371b5c619c780266a35
Liudmyla12 <133156907+Liudmyla12@users.noreply.github.com>
2024-03-31 04:16:46 +0200

    git add .

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,25 +1,29 @@
+import React from 'react';
 import css from './TransactionHistory.module.css';
 
-const TransactionHistory = ({ items }) => (
-  <table className={css.transaction_history}>
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>Amount</th>
-        <th>Currency</th>
-      </tr>
-    </thead>
+const TransactionHistory = ({ items }) => {
+  return (
+    <div className={css.transaction_history_container}>
+      <table className={css.transaction_history}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-    <tbody>
-      {items.map(({ id, type, amount, currency }) => (
-        <tr key={id}>
-          <td>{type}</td>
-          <td>{amount}</td>
-          <td>{currency}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
-
-export { TransactionHistory };
+export default TransactionHistory;
